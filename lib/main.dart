d0afcff64ca988b40a9006e31b4f0b63b2538766
Vitalii <43864798+lost-head@users.noreply.github.com>
2025-03-25 12:17:52 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_expence_tracker/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'simple_bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const MyApp());
+}
+
